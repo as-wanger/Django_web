7e87b118b5639f47330c23c4aeb352601ceb96e5
aswanger <wang.chien.chih.yay@gmail.com>
2022-03-29 22:59:14 +0800

    change TIME_ZONE 3/29

--- Django_web/settings.py
+++ Django_web/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'  # origin -> UTC
 
 USE_I18N = True
 
